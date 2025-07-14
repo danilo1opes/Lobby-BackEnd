56e8da5b87c57602fe5a7ec0b3ecb7c1850e2095
danilo1opes <contatolopesdan@gmail.com>
2025-07-13 22:38:40 -0300

    Teste BackEnd

--- src/models/Photo.ts
+++ src/models/Photo.ts
@@ -1,3 +1,35 @@
+// import mongoose from 'mongoose';
+
+// const photoSchema = new mongoose.Schema(
+//   {
+//     title: { type: String, required: true },
+//     content: { type: String, required: true },
+//     author: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'User',
+//       required: true,
+//     },
+//     src: { type: String, required: true },
+//     peso: { type: Number },
+//     idade: { type: Number },
+//     acessos: { type: Number, default: 0 },
+//   },
+//   { timestamps: true }
+// );
+// interface IPhoto extends mongoose.Document {
+//   title: string;
+//   content: string;
+//   author: mongoose.Types.ObjectId;
+//   src: string;
+//   peso?: number;
+//   idade?: number;
+//   acessos: number;
+//   createdAt?: Date;
+//   updatedAt?: Date;
+// }
+
+// export default mongoose.model<IPhoto>('Photo', photoSchema);
+
 import mongoose from 'mongoose';
 
 const photoSchema = new mongoose.Schema(
@@ -10,19 +42,20 @@ const photoSchema = new mongoose.Schema(
       required: true,
     },
     src: { type: String, required: true },
-    peso: { type: Number },
-    idade: { type: Number },
+    peso: { type: String },
+    idade: { type: String }, 
     acessos: { type: Number, default: 0 },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
+
 interface IPhoto extends mongoose.Document {
   title: string;
   content: string;
   author: mongoose.Types.ObjectId;
   src: string;
-  peso?: number;
-  idade?: number;
+  peso?: string; // Atualizado para string
+  idade?: string; // Atualizado para string
   acessos: number;
   createdAt?: Date;
   updatedAt?: Date;
